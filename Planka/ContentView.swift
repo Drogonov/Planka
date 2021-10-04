60157499d622f38e0756eb4cb05414f2c797b866
Anton Drogonov <vlezko.anton@yandex.ru>
2021-10-04 19:50:30 +0300

    initial commit

---
+++ Planka/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Planka
+//
+//  Created by Anton Vlezko on 04.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Text")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
